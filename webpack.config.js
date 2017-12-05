f1367d746a9d195878e6c35563d5a824f20300b0
Jimmy Nilsson <Jiiiimmmyy@gmail.com>
2017-12-06 00:27:27 +0100

    Config webpack dev-server & add a bunch of loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path');
 
 module.exports = {
-	//entry: ['webpack/hot/dev-server', path.resolve(__dirname, '../src/main.js')],
 	entry: './src/main.js',
 	output: { 
 		path: path.resolve(__dirname, './'),
 		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './'
 	},
 	module: {
 		loaders: [
@@ -14,7 +16,18 @@ module.exports = {
 				loaders: 'babel-loader',
 				exclude: /node_modules/,
 				query: {
-					presets: ['es2015', 'react']
+					presets: ['es2017', 'react']
+				}
+			},
+			{ 
+				test: /\.(css|styl)$/,
+				loader: "style-loader!css-loader!stylus-loader", 
+			},
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000
 				}
 			}
 		]
